440a80c85fefefb6a921695a9f191dd0dd77b0fb
ArturFoden <fodena@tcd.ie>
2024-04-06 19:31:48 +0100

    https://www.youtube.com/watch?v=pJQndtJ2rk0
    Camera improvements, refactored existing logic and added edge panning/click panning
    
    CoAuthor: mckenndy

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,64 +2,73 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour {
+
     // TODO: Create base camera class and inherit
-    public float moveSpeed = 20f;
-    [SerializeField] private float rotationSpeed = 0.1f;
-    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private float moveSpeed = 50f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private int edgeScrollSize = 20;
+    [SerializeField] private float dragPanSpeed = 2f;
+    Vector2 lastMousePosition = new(0, 0);
     private bool increaseSpeed = false;
+    private bool dragPanMoveActive = false;
+
     void Update() {
         RotateCamera();
-        ZoomCamera();
         CameraMovement();
     }
+
     void Start()
     {
         StartCoroutine(IncreaseSpeed());
     }
-        void CameraMovement() {
-        //TODO: Implement drag camera movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+    
+    void CameraMovement() {
 
-        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-        float edgeSize = 30f;
-        if (moveDirection != Vector3.zero) {
-            // Adjust the camera position based on the WASD input
-            transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
-            increaseSpeed = true;
-        }
+        Vector3 inputDir = new(0, 0, 0);
         
-        else if (Input.mousePosition.x > Screen.width - edgeSize) // Right 
-        {
-            Debug.Log(moveSpeed);
-            transform.Translate(new Vector3(1, 0, 0) * moveSpeed * Time.deltaTime, Space.World);
-            increaseSpeed = true;
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - edgeScrollSize) {
+            inputDir.z = +1f;
         }
-        else if (Input.mousePosition.y < edgeSize) // Down
-        {
-            Debug.Log(Screen.width);
-            transform.Translate(new Vector3(0, 0, -1) * moveSpeed * Time.deltaTime, Space.World);
-            increaseSpeed = true;
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y < edgeScrollSize) {
+            inputDir.z = -1f;
         }
-        else if (Input.mousePosition.x < edgeSize) // Left
-        {
-            Debug.Log(Screen.width);
-            transform.Translate(new Vector3(-1, 0, 0) * moveSpeed * Time.deltaTime, Space.World);
-            increaseSpeed = true;
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x < edgeScrollSize) {
+            inputDir.x = -1f;
         }
-        else if (Input.mousePosition.y > Screen.height - edgeSize) // UP
-        {
-            Debug.Log(Screen.width);
-            transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime, Space.World);
-            increaseSpeed = true;
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x > Screen.width - edgeScrollSize) {
+            inputDir.x = +1f;
         }
-        else {
-            increaseSpeed = false;
-            moveSpeed = 20f;
+
+
+        if (Input.GetMouseButtonDown(1)) {
+            dragPanMoveActive = true;
+            lastMousePosition = Input.mousePosition;
         }
+        if (Input.GetMouseButtonUp(1)) {
+            dragPanMoveActive = false;
+        }
+
+        if (dragPanMoveActive) {
+            Vector2 mouseMovementDelta = (Vector2)Input.mousePosition - lastMousePosition;
+
+            inputDir.x = mouseMovementDelta.x * dragPanSpeed;
+            inputDir.z = mouseMovementDelta.y * dragPanSpeed;
+
+            lastMousePosition = Input.mousePosition;
+        }
+
+
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
+
     IEnumerator IncreaseSpeed()
     {
         yield return new WaitForSeconds(.5f);
@@ -73,30 +82,13 @@ public class CameraController : MonoBehaviour {
 
     }
 
-    void ZoomCamera()
-        // TODO: Implement actual zoom
-    {
-        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
-       if (scrollDelta != 0)
-        {
-            Camera.main.fieldOfView -= scrollDelta * zoomSpeed;
-            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 10f, 60f);
 
+    void RotateCamera() {
+        float rotateDir = 0f;
 
+        if (Input.GetKey(KeyCode.E)) rotateDir = +1f;
+        if (Input.GetKey(KeyCode.Q)) rotateDir = -1f;
 
-        }
-    }
-    void RotateCamera()
-    {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(Vector3.down * rotationSpeed);
-
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(Vector3.up * rotationSpeed) ;
-
-        }
+        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
     }
 }
