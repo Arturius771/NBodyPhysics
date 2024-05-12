9eec40e180768b7d8687f054553e9e4fbb1cfb87
ArturFoden <fodena@tcd.ie>
2024-05-12 18:40:34 +0100

    fix build

--- Assets/Scripts/NetworkManagerController.cs
+++ Assets/Scripts/NetworkManagerController.cs
@@ -7,49 +7,49 @@ using UnityEngine.UIElements;
 public class NetworkManagerController : MonoBehaviour {
     [SerializeField] private GameObject uiDocument;
     VisualElement root;
-    Button startHostButton;
-    Button startClientButton;
+    //Button startHostButton;
+    //Button startClientButton;
     Button quitToMenuButton;
-    NetworkManager networkManager;
+    //NetworkManager networkManager;
 
     // TODO - review for improvements and bugs
 
     // Start is called before the first frame update
     void OnEnable() {
         root = uiDocument.GetComponent<UIDocument>().rootVisualElement;
-        startHostButton = root.Q<Button>("StartHostButton");
         quitToMenuButton = root.Q<Button>("QuitToMenuButton");
-        startClientButton = root.Q<Button>("StartClientButton");
-        networkManager = this.gameObject.GetComponent<NetworkManager>();
+        //startHostButton = root.Q<Button>("StartHostButton");
+        //startClientButton = root.Q<Button>("StartClientButton");
+        //networkManager = this.gameObject.GetComponent<NetworkManager>();
 
-        startHostButton.clickable.clicked += OnStartHostButtonClick;
+        //startHostButton.clickable.clicked += OnStartHostButtonClick;
         quitToMenuButton.clickable.clicked += OnQuitToMenuButtonClick;
-        startClientButton.clickable.clicked += OnStartClientButtonClick;
+        //startClientButton.clickable.clicked += OnStartClientButtonClick;
     }
 
-    private void OnStartHostButtonClick() {
-        if(startHostButton.text == "Start Host") {
-            networkManager.StartHost();
-            startHostButton.text = "Stop Host";
-        }
-        else {
-            networkManager.Shutdown();
-            startHostButton.text = "Start Host";
-        }
-    }
+    //private void OnStartHostButtonClick() {
+    //    if(startHostButton.text == "Start Host") {
+    //        networkManager.StartHost();
+    //        startHostButton.text = "Stop Host";
+    //    }
+    //    else {
+    //        networkManager.Shutdown();
+    //        startHostButton.text = "Start Host";
+    //    }
+    //}
 
     private void OnQuitToMenuButtonClick() {
         SceneManager.LoadScene("4PlaytestMenu");
     }
 
-    private void OnStartClientButtonClick() {
-        if (startHostButton.text == "Start Client") {
-            networkManager.StartClient();
-            startHostButton.text = "Stop Client";
-        }
-        else {
-            networkManager.Shutdown();
-            startHostButton.text = "Start Client";
-        }
-    }
+    //private void OnStartClientButtonClick() {
+    //    if (startHostButton.text == "Start Client") {
+    //        networkManager.StartClient();
+    //        startHostButton.text = "Stop Client";
+    //    }
+    //    else {
+    //        networkManager.Shutdown();
+    //        startHostButton.text = "Start Client";
+    //    }
+    //}
 }
